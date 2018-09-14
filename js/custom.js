fcb8b352a24afbf86a249863123f8e4e90703c14
Betul Gokbel <gokbel.betul@metu.edu.tr>
2018-09-14 16:54:50 +0300

    all converter is done

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,4 @@
-var url = setInterval(func, 10)																																																																																																												;
+var url = setInterval(func, 100)																																																																																																												;
 
 function func(){
 	let url = 'https://www.doviz.com/api/v1/currencies/all/latest';
@@ -24,11 +24,36 @@ $(document).ready(function(){
 		var usd_value = $("#dollar-input").val();
 		var euro_value = $("#euro-input").val();
 
-        if ( $("#tl-input").val() !=  null ){		
+        if ( $("#dollar-input").val() !=  null ){		
 			document.getElementById("tl-input").placeholder = usd_value*document.getElementById("selling-usd").innerHTML;
 			document.getElementById("euro-input").placeholder = (usd_value*document.getElementById("selling-usd").innerHTML)/document.getElementById("selling-euro").innerHTML;																																																																																																																						
 		}
+    });
 
+    $("#tl-input").keyup(function(){
+    	       
+        var tl_value = $("#tl-input").val();
+		var usd_value = $("#dollar-input").val();
+		var euro_value = $("#euro-input").val();
+
+		if ( $("#tl-input").val() !=  null ){		
+			document.getElementById("dollar-input").placeholder = tl_value/document.getElementById("selling-usd").innerHTML;
+			document.getElementById("euro-input").placeholder = tl_value/document.getElementById("selling-euro").innerHTML;																																																																																																												
+		}
+
+    });
+
+    $("#euro-input").keyup(function(){
+    	       
+        var tl_value = $("#tl-input").val();
+		var usd_value = $("#dollar-input").val();
+		var euro_value = $("#euro-input").val();
+		console.log(usd_value);
+
+		if ( $("#euro-input").val() !=  null ){		
+			document.getElementById("dollar-input").placeholder = (euro_value*document.getElementById("selling-euro").innerHTML)/document.getElementById("selling-usd").innerHTML;
+			document.getElementById("tl-input").placeholder =euro_value*document.getElementById("selling-euro").innerHTML;																																																																																																												
+		}
 
     });
 });
