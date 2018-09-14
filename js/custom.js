e5e9ff45d35ace1513d43315649a5f9f7ed99ccb
Betul Gokbel <gokbel.betul@metu.edu.tr>
2018-09-14 16:10:15 +0300

    adding converter for dollar

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,34 @@
-var url = setInterval(func, 10);
+var url = setInterval(func, 10)																																																																																																												;
 
 function func(){
 	let url = 'https://www.doviz.com/api/v1/currencies/all/latest';
 	fetch(url)
 	.then(res => res.json())
 	.then((result) => {
-		document.getElementById("selling-usd").innerHTML = result[0]['selling']; /*DOLLOR*/
-		document.getElementById("buying-usd").innerHTML = result[0]['buying'];	 /*DOLLOR*/
-		document.getElementById("selling-euro").innerHTML = result[1]['selling']; /*EURO*/
-		document.getElementById("buying-euro").innerHTML = result[1]['buying'];	 /*EURO*/
+		document.getElementById("selling-usd").innerHTML = result[0]['selling']; /*DOLLOR SELLING*/
+		document.getElementById("buying-usd").innerHTML = result[0]['buying'];	 /*DOLLOR BUYING*/
+		document.getElementById("selling-euro").innerHTML = result[1]['selling']; /*EURO SELLING*/
+		document.getElementById("buying-euro").innerHTML = result[1]['buying'];	 /*EURO BUYING*/
 
+		var tl_value = $("#tl-input").val();
+		var usd_value = $("#dollar-input").val();
+		var euro_value = $("#euro-input").val();
 	})
-	let old_dollar = result[0]['selling'];
-	console.log("------------");
-	console.log(old_dollar);	
-}
+}																					
 
+$(document).ready(function(){
+
+    $("#dollar-input").keyup(function(){
+    	       
+        var tl_value = $("#tl-input").val();
+		var usd_value = $("#dollar-input").val();
+		var euro_value = $("#euro-input").val();
+
+        if ( $("#tl-input").val() !=  null ){		
+			document.getElementById("tl-input").placeholder = usd_value*document.getElementById("selling-usd").innerHTML;
+			document.getElementById("euro-input").placeholder = (usd_value*document.getElementById("selling-usd").innerHTML)/document.getElementById("selling-euro").innerHTML;																																																																																																																						
+		}
+
+
+    });
+});
